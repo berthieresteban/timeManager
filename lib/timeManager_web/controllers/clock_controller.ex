57dfae8385988af624320fdb97d8ee91498feb22
Julien <julien.legay@epitech.eu>
2019-09-09 17:23:01 +0200

    Elixir project | Creating + Schemas + Routing

---
+++ lib/timeManager_web/controllers/clock_controller.ex
@@ -0,0 +1,43 @@
+defmodule TimeManagerWeb.ClockController do
+  use TimeManagerWeb, :controller
+
+  alias TimeManager.Auth
+  alias TimeManager.Auth.Clock
+
+  action_fallback TimeManagerWeb.FallbackController
+
+  def index(conn, _params) do
+    clocks = Auth.list_clocks()
+    render(conn, "index.json", clocks: clocks)
+  end
+
+  def create(conn, %{"clock" => clock_params}) do
+    with {:ok, %Clock{} = clock} <- Auth.create_clock(clock_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", Routes.clock_path(conn, :show, clock))
+      |> render("show.json", clock: clock)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    clock = Auth.get_clock!(id)
+    render(conn, "show.json", clock: clock)
+  end
+
+  def update(conn, %{"id" => id, "clock" => clock_params}) do
+    clock = Auth.get_clock!(id)
+
+    with {:ok, %Clock{} = clock} <- Auth.update_clock(clock, clock_params) do
+      render(conn, "show.json", clock: clock)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    clock = Auth.get_clock!(id)
+
+    with {:ok, %Clock{}} <- Auth.delete_clock(clock) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
