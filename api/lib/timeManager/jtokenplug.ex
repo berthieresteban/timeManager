7290f207bc51a62c70d886dc70482213a296ed6e
Julien <julien.legay@epitech.eu>
2019-09-19 13:42:34 +0200

    Permission plug connected

--- api/lib/timeManager/jtokenplug.ex
+++ api/lib/timeManager/jtokenplug.ex
@@ -2,21 +2,21 @@ defmodule TimeManager.JwtAuthPlug do
     import Plug.Conn
 
     alias TimeManager.Token
+    alias TimeManager.Auth
+    alias TimeManager.Permission
 
     def init(opts), do: opts
 
     def call(conn, _opts) do
         token = jwt_from_header(conn)
         useropen = List.last(conn.path_info)
-        if (useropen != "users") do
-            case Token.verify_and_validate(token) do
-                {:ok, claims} ->
-                conn |> success(claims, token)
-                { :error, _error } ->
-                conn |> forbidden
-            end
-        else
-            conn
+        case Token.verify_and_validate(token) do
+            {:ok, claims} ->
+            conn 
+            |> success(claims, token)
+            |> Permission.hasPermission
+            { :error, _error } ->
+            conn |> forbidden
         end
     end
 
@@ -30,8 +30,9 @@ defmodule TimeManager.JwtAuthPlug do
     end
 
     defp success(conn, token_payload, token) do
-        assign(conn, :claims, token_payload)
-        |> assign(:jwt, token)    
+        assign(conn, :perm, token_payload["perm"])
+        |> assign(:user, token_payload["user"])
+        |> assign(:jwt, token)
     end
 
     defp forbidden(conn) do
