c9e68502f67c466cb895ef4f27ea02a33d8a16ea
Julien <julien.legay@epitech.eu>
2019-09-19 09:50:15 +0200

    Code cleanup

--- api/lib/timeManager_web/views/auth_view.ex
+++ api/lib/timeManager_web/views/auth_view.ex
@@ -1,14 +1,5 @@
 defmodule TimeManagerWeb.AuthView do
   use TimeManagerWeb, :view
-  alias TimeManagerWeb.AuthView
-
-  def render("index.json", %{token: token}) do
-    %{data: render_many(token, AuthView, "token.json")}
-  end
-
-  def render("show.json", %{status: status, token: token}) do
-    %{data: render_one(token, AuthView, "token.json")}
-  end
 
   def render("token.json", %{status: status, token: token, user: user}) do
     %{status: status, token: token, user: user}
