6fa6cc4fef1c4bbad165201380de9ebe43c397f5
Julien <julien.legay@epitech.eu>
2019-09-19 11:07:13 +0200

    Code cleanup + password admin bug fix

--- api/lib/timeManager/auth.ex
+++ api/lib/timeManager/auth.ex
@@ -58,8 +58,6 @@ defmodule TimeManager.Auth do
 
   def get_user_login(username, password) do
     password = Base.encode16(:crypto.hash(:sha256,  "#{password}_s3cr3tp4s$xXxX_______try_to_crack_this_lol"))
-    IO.inspect(username)
-    IO.inspect(password)
     query = from u in User, where: u.username == ^username, where: u.password == ^password
     Repo.all(query)
   end
@@ -77,8 +75,8 @@ defmodule TimeManager.Auth do
 
   """
   def create_user(attrs \\ %{}) do
-    password = password = Base.encode16(:crypto.hash(:sha256,  "#{attrs["password"]}_s3cr3tp4s$xXxX_______try_to_crack_this_lol"))
-    IO.inspect(password)
+    npass = is_nil(attrs["password"]) && attrs.password || attrs["password"]
+    password = Base.encode16(:crypto.hash(:sha256,  "#{npass}_s3cr3tp4s$xXxX_______try_to_crack_this_lol"))
     %User{}
     |> User.changeset(attrs)
     |> User.changeset(%{password: password})
