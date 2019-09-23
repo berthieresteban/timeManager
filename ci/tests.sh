e0aa069fda564de9db0c61c2229b4cc721aafddd
Berthier <Berthier.est@gmail.com>
2019-09-23 16:02:54 +0200

    ugly hack travis

--- ci/tests.sh
+++ ci/tests.sh
@@ -6,7 +6,8 @@ docker-compose -f ./docker-compose.yml up -d
 
 printf '\nStarting backend\n'
 
-until curl --output /dev/null --silent --head --fail http://localhost:4000/api/auth; do
+# Ugly hack to check if backend started  / Need to change with a custom route 
+until curl --output /dev/null --silent --fail -d '{"auth": { "username": "lol", "password": "lol"}}' -H "Content-Type: application/json" -X POST http://localhost:4000/auth; do
   printf '.'
   sleep 5
 done
