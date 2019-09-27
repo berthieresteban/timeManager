0f3362a3c9091f115b46af8c6094ffca45222ab7
Berthier <esteban.berthier@epitech.eu>
2019-09-27 14:04:00 +0200

    tests front

---
+++ frontend/cypress/integration/administrator.spec.js
@@ -0,0 +1,126 @@
+
+describe('administrator tests', () => {
+
+  it('should log in', function () {
+        cy.fixture(`administrator.json`).as('fixt')
+        .then(() => {
+          cy.visit('/');
+          cy.contains('Username').parent().children().first().next().type(this.fixt.username);
+          cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+          cy.get('.v-card__actions').children().first().click();
+        })
+  });
+
+  it('Should Clock IN / Clock OUT', function () {
+    cy.fixture(`administrator.json`).as('fixt')
+    .then(() => {
+      cy.visit('/');
+      cy.contains('Username').next().type(this.fixt.username);
+      cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+      cy.get('.v-card__actions').children().first().click();
+    }).then(() => {
+      cy.contains("Clock'")
+    }).then(() => {
+      cy.contains("Clock'")
+    })
+  });
+
+  it('Should Display good Informations', function () {
+    cy.fixture(`administrator.json`).as('fixt')
+    .then(() => {
+      cy.visit('/');
+      cy.contains('Username').parent().children().first().next().type(this.fixt.username);
+      cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+      cy.get('.v-card__actions').children().first().click();
+    }).then(() => {
+      cy.get('.fa-bars').click();
+    }).then(() => {
+      cy.contains(this.fixt.email)
+    }).then(() => {
+      cy.contains(this.fixt.username)
+    }).then(() => {
+      cy.contains("Administrator TimeManager")
+    })
+  });
+
+  it('Should Set Dark Mode', function () {
+    cy.fixture(`administrator.json`).as('fixt')
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').next().type(this.fixt.username);
+        cy.contains('Password').next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.get('.fa-bars').click();
+      }).then(() => {
+        cy.get('[type="checkbox"]').next().click()
+      }).then(() => {
+        cy.get('.hello').should('have.css', 'color').and('equal', 'rgb(44, 62, 80)')
+      })
+  });
+
+  it('Should access WorkingTimes', function () {
+    cy.fixture(`administrator.json`).as('fixt')
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').next().type(this.fixt.username);
+        cy.contains('Password').next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.get('.fa-bars').click();
+      }).then(() => {
+        cy.contains('Working Times').click()
+      }).then(() => {
+        cy.contains(`Dashboard of ${this.fixt.username}`);
+      })
+  });
+
+  it('Should Access Chart Management', function () {
+    cy.fixture(`administrator.json`).as('fixt')
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').next().type(this.fixt.username);
+        cy.contains('Password').next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.get('.fa-bars').click();
+      }).then(() => {
+        cy.contains('Chart Manager').click()
+      }).then(() => {
+        cy.contains('Area Chart');
+        cy.contains('Bar Chart');
+        cy.contains('Line Chart');
+        cy.contains('Donut Chart');
+      })
+  });
+
+  it('Should Access Edit Account', function () {
+    cy.fixture(`administrator.json`).as('fixt')
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').next().type(this.fixt.username);
+        cy.contains('Password').next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.get('.fa-bars').click();
+      }).then(() => {
+        cy.contains('Edit Account').click()
+      }).then(() => {
+        cy.contains('Update Account Informations');
+      })
+  });
+
+  it('Should Access Accounts Management', function () {
+    cy.fixture(`administrator.json`).as('fixt')
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').next().type(this.fixt.username);
+        cy.contains('Password').next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.contains('Manage Accounts').click()
+      }).then(() => {
+        cy.contains('Create Account');
+      })
+  });
+});
