539e5a4705f4a8dfcaf1e54bd432761b373bf45c
Berthier <esteban.berthier@epitech.eu>
2019-09-27 16:14:41 +0200

    front

--- frontend/cypress/integration/administrator.spec.js
+++ frontend/cypress/integration/administrator.spec.js
@@ -1,50 +1,53 @@
 
 describe('administrator tests', () => {
-  beforeEach(()=> {
+  beforeEach(() => {
     cy.clearLocalStorage()
     cy.wait(2000);
   });
 
   it('should log in', function () {
-        cy.fixture(`administrator.json`).as('fixt')
-        .then(() => {
-          cy.visit('/');
-          cy.contains('Username').parent().children().first().next().type(this.fixt.username);
-          cy.contains('Password').parent().children().first().next().type(this.fixt.password);
-          cy.get('.v-card__actions').children().first().click();
-        })
+    cy.fixture(`administrator.json`).as('fixt')
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').parent().children().first().next().type(this.fixt.username);
+        cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      })
+    cy.wait(2000);
   });
 
   it('Should Clock IN / Clock OUT', function () {
     cy.fixture(`administrator.json`).as('fixt')
-    .then(() => {
-      cy.visit('/');
-      cy.contains('Username').next().type(this.fixt.username);
-      cy.contains('Password').parent().children().first().next().type(this.fixt.password);
-      cy.get('.v-card__actions').children().first().click();
-    }).then(() => {
-      cy.contains("Clock'")
-    }).then(() => {
-      cy.contains("Clock'")
-    })
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').next().type(this.fixt.username);
+        cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.contains("Clock'")
+      }).then(() => {
+        cy.contains("Clock'")
+      })
+    cy.wait(2000);
   });
 
   it('Should Display good Informations', function () {
     cy.fixture(`administrator.json`).as('fixt')
-    .then(() => {
-      cy.visit('/');
-      cy.contains('Username').parent().children().first().next().type(this.fixt.username);
-      cy.contains('Password').parent().children().first().next().type(this.fixt.password);
-      cy.get('.v-card__actions').children().first().click();
-    }).then(() => {
-      cy.get('.fa-bars').click();
-    }).then(() => {
-      cy.contains(this.fixt.email)
-    }).then(() => {
-      cy.contains(this.fixt.username)
-    }).then(() => {
-      cy.contains("Administrator TimeManager")
-    })
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').parent().children().first().next().type(this.fixt.username);
+        cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.get('.fa-bars').click();
+      }).then(() => {
+        cy.contains(this.fixt.email)
+      }).then(() => {
+        cy.contains(this.fixt.username)
+      }).then(() => {
+        cy.contains("Administrator TimeManager")
+      })
+    cy.wait(2000);
   });
 
   it('Should Set Dark Mode', function () {
@@ -61,6 +64,7 @@ describe('administrator tests', () => {
       }).then(() => {
         cy.get('.hello').should('have.css', 'color').and('equal', 'rgb(44, 62, 80)')
       })
+    cy.wait(2000);
   });
 
   it('Should access WorkingTimes', function () {
@@ -77,6 +81,7 @@ describe('administrator tests', () => {
       }).then(() => {
         cy.contains(`Dashboard of ${this.fixt.username}`);
       })
+    cy.wait(2000);
   });
 
   it('Should Access Chart Management', function () {
@@ -96,6 +101,7 @@ describe('administrator tests', () => {
         cy.contains('Line Chart');
         cy.contains('Donut Chart');
       })
+    cy.wait(2000);
   });
 
   it('Should Access Edit Account', function () {
@@ -112,6 +118,7 @@ describe('administrator tests', () => {
       }).then(() => {
         cy.contains('Update Account Informations');
       })
+    cy.wait(2000);
   });
 
   it('Should Access Accounts Management', function () {
@@ -126,5 +133,6 @@ describe('administrator tests', () => {
       }).then(() => {
         cy.contains('Create Account');
       })
+    cy.wait(2000);
   });
 });
--- frontend/cypress/integration/manager.spec.js
+++ frontend/cypress/integration/manager.spec.js
@@ -3,9 +3,10 @@ describe('manager tests', () => {
 
   before(() => {
     cy.createUser('manager')
+    cy.wait(2000);
   });
 
-  beforeEach(()=> {
+  beforeEach(() => {
     cy.clearLocalStorage()
     cy.wait(2000);
   });
@@ -18,38 +19,41 @@ describe('manager tests', () => {
         cy.contains('Password').parent().children().first().next().type(this.fixt.password);
         cy.get('.v-card__actions').children().first().click();
       })
+    cy.wait(2000);
   });
 
   it('Should Clock IN / Clock OUT', function () {
     cy.fixture(`manager.json`).as('fixt')
-    .then(() => {
-      cy.visit('/');
-      cy.contains('Username').parent().children().first().next().type(this.fixt.username);
-      cy.contains('Password').parent().children().first().next().type(this.fixt.password);
-      cy.get('.v-card__actions').children().first().click();
-    }).then(() => {
-      cy.contains("Clock'")
-    }).then(() => {
-      cy.contains("Clock'")
-    })
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').parent().children().first().next().type(this.fixt.username);
+        cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.contains("Clock'")
+      }).then(() => {
+        cy.contains("Clock'")
+      })
+    cy.wait(2000);
   });
 
   it('Should Display good Informations', function () {
     cy.fixture(`manager.json`).as('fixt')
-    .then(() => {
-      cy.visit('/');
-      cy.contains('Username').parent().children().first().next().type(this.fixt.username);
-      cy.contains('Password').parent().children().first().next().type(this.fixt.password);
-      cy.get('.v-card__actions').children().first().click();
-    }).then(() => {
-      cy.get('.fa-bars').click();
-    }).then(() => {
-      cy.contains(this.fixt.email)
-    }).then(() => {
-      cy.contains(this.fixt.username)
-    }).then(() => {
-      cy.contains("Manager TimeManager")
-    })
+      .then(() => {
+        cy.visit('/');
+        cy.contains('Username').parent().children().first().next().type(this.fixt.username);
+        cy.contains('Password').parent().children().first().next().type(this.fixt.password);
+        cy.get('.v-card__actions').children().first().click();
+      }).then(() => {
+        cy.get('.fa-bars').click();
+      }).then(() => {
+        cy.contains(this.fixt.email)
+      }).then(() => {
+        cy.contains(this.fixt.username)
+      }).then(() => {
+        cy.contains("Manager TimeManager")
+      })
+    cy.wait(2000);
   });
 
   it('Should Set Dark Mode', function () {
@@ -66,6 +70,7 @@ describe('manager tests', () => {
       }).then(() => {
         cy.get('.hello').should('have.css', 'color').and('equal', 'rgb(44, 62, 80)')
       })
+    cy.wait(2000);
   });
 
   it('Should access WorkingTimes', function () {
@@ -82,6 +87,7 @@ describe('manager tests', () => {
       }).then(() => {
         cy.contains(`Dashboard of ${this.fixt.username}`);
       })
+    cy.wait(2000);
   });
 
   it('Should Access Chart Management', function () {
@@ -101,6 +107,7 @@ describe('manager tests', () => {
         cy.contains('Line Chart');
         cy.contains('Donut Chart');
       })
+    cy.wait(2000);
   });
 
   it('Should Access Edit Account', function () {
@@ -117,5 +124,6 @@ describe('manager tests', () => {
       }).then(() => {
         cy.contains('Update Account Informations');
       })
+    cy.wait(2000);
   });
 });
